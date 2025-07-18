f8220eaf8a42ddbbd92ded3472d35cc0d42780d6
HareeshwarChowdary <158481126+HareeshwarChowdary@users.noreply.github.com>
2025-07-18 10:43:18 +0530

    Add files via upload

---
+++ js/medicines.js
@@ -0,0 +1,37 @@
+const medicines = [
+    {
+        id: 1,
+        name: "Paracetamol",
+        price: 30,
+        use: "Fever, Pain Relief",
+        image: "images/paracetamol.jpg"
+    },
+    {
+        id: 2,
+        name: "Azithromycin",
+        price: 150,
+        use: "Bacterial Infections",
+        image: "images/azithromycin.jpg"
+    },
+    {
+        id: 3,
+        name: "Ibuprofen",
+        price: 50,
+        use: "Pain Relief, Anti-inflammatory",
+        image: "images/ibuprofen.jpg"
+    },
+    {
+        id: 4,
+        name: "Cetirizine",
+        price: 40,
+        use: "Allergy Relief",
+        image: "images/cetirizine.jpg"
+    },
+    {
+        id: 5,
+        name: "Amoxicillin",
+        price: 120,
+        use: "Bacterial Infections",
+        image: "images/amoxicillin.jpg"
+    }
+];
